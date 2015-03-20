66133450b58d71e49934ea1da0e43fe5a33d94a7
Carlton Shepherd <carlton@linux.com>
2015-03-20 11:17:26 +0000

    Added challenges from page 57

--- little-schemer.scm
+++ little-schemer.scm
@@ -1,11 +1,15 @@
-;; Little schemer commandments:
+;; The Little Schemer commandments:
 ;; 1. The first condition when recursing through a list should be (null? lat)
 ;; 2. Use 'cons' to build lists
 ;; 3. When building a list, describe the first typical element and cons it onto the recursion
+;; 4. Always change at least one argument when recurring.  It must be changed to be closer to termination.
+;;    The changing argument must be tested in the termination condition;
+;;    for example, when using cdr, test termination with null?
 
 (define atom?
   (lambda (x)
-    (and (not (pair? x)) (not (null? x)))))
+    (and
+     (not (pair? x)) (not (null? x)))))
 
 (define lat?
   (lambda (l)
@@ -66,3 +70,55 @@
 		   (insertR new old
 			    (cdr lat)))))))))
 
+;; multirember: Removes all occurrences of an element from list 'lat'
+;; Page 53
+(define multirember
+  (lambda (a lat)
+    (cond
+     ((eq? (car lat) a) (multirember a (cdr lat)))
+     (else (cons (car lat)
+		 (multirember a (cdr lat)))))))
+
+;; multiinsertR: Inserts element 'new' to the right of every occurrence of 'old' in list 'lat'
+;; Page 56
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old)
+	(cons (car lat)
+	      (cons new
+		    (multiinsertR new old (cdr lat)))))
+       (else (cons (car lat)
+		   (multiinsertR new old (cdr lat)))))))))
+
+;; multiinsertL: Inserts element 'new' to the left of every occurrence of 'old' in list 'lat'
+;; Page 57
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old)
+	(cons new
+	      ;; 'old' is equivalent to (car lat) in this case
+	      (cons old 
+			(multiinsertL new old (cdr lat)))))
+       (else (cons (car lat)
+		   (multiinsertL new old (cdr lat)))))))))
+
+;; multisubst: Replaces all occurrences of element 'old' in list 'lat' with 'new'
+;; Page 57
+(define multisubst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((eq? (car lat) old)
+	     (cons new
+		   (multisubst new old (cdr lat))))
+	    (else (cons (car lat)
+			(multisubst new old (cdr lat)))))))))
