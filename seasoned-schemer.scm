ddcc84daedad8a6a450ae5d01d39914fd27d710d
Carlton Shepherd <carlton.shepherd.2014@live.rhul.ac.uk>
2015-04-09 18:52:37 +0100

    Added functions up to ch.2, p.15

--- seasoned-schemer.scm
+++ seasoned-schemer.scm
@@ -12,7 +12,7 @@
   (lambda (lat)
     (cond
      ((null? lat) #f)
-     (or (is-first-b? (car lat) (cdr lat))))))
+     (else (or (is-first-b? (car lat) (cdr lat)))))))
 
 ;; is-first-b?: Helper function to two-in-a-row?
 ;; Page 6
@@ -39,4 +39,53 @@
     (cond
      ((null? lat) #f)
      (else two-in-a-row-b? (car lat) (cdr lat)))))
+
+;; sum-of-prefixes: For a given list, returns a new list such that each
+;; element is the sum of that element and all of its preceding elements
+;; Page 9-11
+(define sum-of-prefixes
+  (lambda (tup)
+    (sum-of-prefixes-b 0 tup)))
+
+;; sum-of-prefixes-b: Helper function to sum-of-prefixes: returns a list
+;; where each element is the cumulative sum of each element of the
+;; argument list
+;; Page 10
+(define sum-of-prefixes-b
+  (lambda (sonssf tup)
+    (cond
+     ((null? tup) (quote ()))
+     (else (cons (+ sonssf (car tup))
+		 (sum-of-prefixes-b (+ sonssf (car tup))
+				    (cdr tup)))))))
+
+;; scramble: Takes a tup in which no number is greater than its index.  Each
+;; number is treated as a reference to a previous index; the result at each
+;; position is found by counting backward from the current position according
+;; to this index (i.e. position minus index)
+;; Page 15
+(define scramble
+  (lambda (tup)
+    (scramble-b tup (quote ()))))
+
+;; pick: Retrieves the nth element from a given list
+;; Page 13
+(define pick
+  (lambda (n lat)
+    (cond
+     ((eq? n 1) (car lat))
+     (else (pick (- n 1)
+		 (cdr lat))))))
+
+;; scramble-b: Helper function to scramble
+;; Page 14
+(define scramble-b
+  (lambda (tup rev-pre)
+    (cond
+     ((null? tup) (quote ()))
+     (else
+      (cons (pick (car tup)
+		  (cons (car tup) rev-pre))
+	    (scramble-b (cdr tup)
+			(cons (car tup) rev-pre)))))))
 
